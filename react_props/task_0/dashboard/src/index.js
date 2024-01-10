71d9f03085a3cf28546dc35996019c8bed0ec2c7
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-10 08:36:48 -0600

    removed root notifications shell

--- react_props/task_0/dashboard/src/index.js
+++ react_props/task_0/dashboard/src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import Notifications from './Notifications/Notifications';
+
 
 // Root App
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
-
-// Root Notifications
-const rootNotif = ReactDOM.createRoot(
-  document.getElementById("root-notifications")
-);
-rootNotif.render(
-  <React.StrictMode>
-    <Notifications />
   </React.StrictMode>
 );
