3f3310c2f698ef34846b8e9ee648c2adce686c4b
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-10 20:00:19 -0600

    added corrsct path for testing

--- react_props/task_1/dashboard/src/App/App.test.js
+++ react_props/task_1/dashboard/src/App/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-import Notifications from './Notifications/Notifications';
-import Header from './Header/Header';
-import Login from './Login/Login';
-import Footer from './Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
 
 describe('<App />', () => {
   it('contains the Notifications component', () => {
