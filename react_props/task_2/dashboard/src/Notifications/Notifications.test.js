f7c6e248e78528b4bdb497f9e4a3e0dac41dbc12
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-10 20:15:08 -0600

    task2

--- react_props/task_2/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_2/dashboard/src/Notifications/Notifications.test.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Notifications from './Notifications';
+import NotificationItem from './NotificationItem';
 
 describe('Notifications', () => {
-  it('renders without crashing', () => {
-    shallow(<Notifications />);
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Notifications />);
   });
 
-  it('renders three list items', () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.find('li').length).toBe(3);
+  it('renders without crashing', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  // check for NotificationItem components
+  it('renders NotificationItem components', () => {
+    expect(wrapper.find(NotificationItem).length).toBe(3);
   });
 
   it('renders the text Here is the list of notifications', () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.find('p').text()).toContain('Here is the list of notifications');
+    expect(wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)).toBe(true);
+  });
+
+  // New test for checking correct rendering of NotificationItem elements
+  it('first NotificationItem renders the right html', () => {
+    const firstItem = wrapper.find(NotificationItem).first();
+    expect(firstItem.props().value).toEqual('New course available');
+    expect(firstItem.props().type).toEqual('default');
   });
 });
