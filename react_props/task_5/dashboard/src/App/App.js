fae2438db90cdcdc23de0d8e39a6c36815e63c72
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-10 21:43:29 -0600

    created list course array

--- react_props/task_5/dashboard/src/App/App.js
+++ react_props/task_5/dashboard/src/App/App.js
@@ -8,16 +8,29 @@ import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
 
 function App({ isLoggedIn }) {
+
+  const listCourses = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
+  ];
+
+  const listNotifications = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+  ]
+
   return (
     <>
     <div className="App-header-wrapper">
       <Header />
       <div className="App-menu">
-        <Notifications />
+        <Notifications listNotifications={listNotifications} />
       </div>
       </div>
       <div className="App">
-        {isLoggedIn ? <CourseList /> : <Login />}
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
       </div>
       <div>
         <Footer />
