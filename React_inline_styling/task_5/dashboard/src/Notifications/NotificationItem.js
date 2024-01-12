344acfb22654989fad392f03fbfa62fa7af903f0
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-12 16:40:04 -0600

    adding files from task 5 per task

---
+++ React_inline_styling/task_5/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,36 @@
+import React, { Component, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends PureComponent {
+  render() {
+    const { id, type, value, html, markAsRead } = this.props;
+
+    return (
+      <li
+      onClick={() => markAsRead(id)}
+      data-notification-type={type}
+      dangerouslySetInnerHTML={html}>
+      {value}
+      </li>
+    )
+  }
+}
+
+NotificationItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  markAsRead: PropTypes.func.isRequired,
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  value: '',
+  html: null,
+  markAsRead: () => {},
+};
+
+export default NotificationItem;
