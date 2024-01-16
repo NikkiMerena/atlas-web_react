78cb6f160b52d4a63a559e51739e121f92df45a8
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-15 21:36:12 -0600

    adding test

--- React_inline_styling/task_0/dashboard/config/setupTests.js
+++ React_inline_styling/task_0/dashboard/config/setupTests.js
@@ -1,12 +1,8 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+import 'fast-text-encoding';
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { createSerializer } from 'enzyme-to-json'
+import { createSerializer } from 'enzyme-to-json';
 
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
-
 configure({ adapter: new Adapter() });
