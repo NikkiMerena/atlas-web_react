2a5759fcd4074b3695d79c1940e54c588983dc3f
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-19 12:04:20 -0600

    adding task files

---
+++ React_inline_styling/task_2/dashboard/config/setupTests.js
@@ -0,0 +1,8 @@
+import 'fast-text-encoding';
+import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { createSerializer } from 'enzyme-to-json';
+
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+configure({ adapter: new Adapter() });
