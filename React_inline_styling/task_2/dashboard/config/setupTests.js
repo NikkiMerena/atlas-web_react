81e31a1638cef60782a995c8eab6ef55e3f0c253
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-19 13:12:09 -0600

    added import

--- React_inline_styling/task_2/dashboard/config/setupTests.js
+++ React_inline_styling/task_2/dashboard/config/setupTests.js
@@ -3,6 +3,11 @@ import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { createSerializer } from 'enzyme-to-json';
+import { StyleSheetTestUtils } from 'aphrodite';
 
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
 configure({ adapter: new Adapter() });
+
+if (StyleSheetTestUtils && StyleSheetTestUtils.suppressStyleInjection) {
+  StyleSheetTestUtils.suppressStyleInjection();
+}
