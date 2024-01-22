17f2a91d0710736b4e1dcb08726ad4bf3b5f0ad8
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-21 22:45:44 -0600

    updated enzyme adapter, hoping it helps

--- react_state/task_0/dashboard/config/setupTests.js
+++ react_state/task_0/dashboard/config/setupTests.js
@@ -1,7 +1,7 @@
 import 'fast-text-encoding';
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import { createSerializer } from 'enzyme-to-json';
 import { StyleSheetTestUtils } from 'aphrodite';
 
