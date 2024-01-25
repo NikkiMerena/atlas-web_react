ec56397a5787b0e2b362fcb75fcb3f046629b03a
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-25 11:16:32 -0600

    still on task2

--- react_state/task_2/dashboard/src/Header/Header.js
+++ react_state/task_2/dashboard/src/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../assets/atlas_logo.png';
 import { StyleSheet, css } from 'aphrodite';
+import { AppContext } from '../App/AppContext';
 
 const styles = StyleSheet.create({
 
@@ -25,15 +26,26 @@ const styles = StyleSheet.create({
     },
     });
 
-    function Header() {
-        return (
-        <header className={css(styles.headerMain)}>
-            <img src={logo} className={css(styles.logo)} alt="logo" />
-            <h1 className={css(styles.headerTitle)}>
+    class Header extends React.Component {
+        static contextType = AppContext;
+
+        render() {
+            const { user } = this.context;
+
+            return (
+            <header className={css(styles.headerMain)}>
+                <img src={logo} className={css(styles.logo)} alt="logo" />
+                <h1 className={css(styles.headerTitle)}>
                 School dashboard
             </h1>
-        </header>
-        );
-}
+                {user.isLoggedIn && (
+                <div id="logoutSection">
+                Welcome {user.email} (<a href="#" onClick={this.context.logOut}>logout</a>)
+                </div>
+            )}
+            </header>
+            );
+        }
+    }
 
-export default Header;
+    export default Header;
