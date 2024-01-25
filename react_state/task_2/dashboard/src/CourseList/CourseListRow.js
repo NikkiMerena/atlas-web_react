74527ea0bc81a9822f3720e5cd5d287b6903f36b
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-24 22:55:10 -0600

    fixing test

--- react_state/task_2/dashboard/src/CourseList/CourseListRow.js
+++ react_state/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,83 +1,96 @@
-// CourseListRow.js - React component that accepts three props: textFirstCell, textSecondCell, and isHeader.
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
-const styles = StyleSheet.create({
-
-    headerRowSpan2: {
-      textAlign: 'center',
-    },
-
-    secondHeaderRow: {
-      borderTop: '2px solid #e0e0e0',
-      borderBottom: '2px solid #e0e0e0',
-    },
-
-    secondHeaderRowFirstCell: {
-      textAlign: 'left',
-      width: '70%',
-    },
-
-    secondHeaderRowSecondCell: {
-      textAlign: 'left',
-      width: '30%',
-    },
-
-    bodyRowCell: {
-      textAlign: 'left',
-    },
-
-    row: {
-      backgroundColor: '#f5f5f5ab',
-    },
-
-    header: {
-      backgroundColor: '#deb5b545',
-    },
-  });
-
-function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
-    // Apply styles condionally
-    const style = isHeader ? headerRowStyle : rowStyle;
-
-    if (isHeader) {
-        if (textSecondCell === null) {
-            return (
-                <tr className={css(style, styles.headerRowSpan2)}>
-                    <th colSpan="2">{textFirstCell}</th>
-                </tr>
-            );
-        } else {
-            return (
-                <tr className={css(style, styles.secondHeaderRow)}>
-          <th className={css(styles.secondHeaderRowFirstCell)}>{textFirstCell}</th>
-          <th className={css(styles.secondHeaderRowSecondCell)}>{textSecondCell}</th>
-        </tr>
-            );
-        }
-    } else {
-        return (
-            <tr className={css(style)}>
-            <td className={css(styles.bodyRowCell)}>{textFirstCell}</td>
-            <td className={css(styles.bodyRowCell)}>{textSecondCell}</td>
-          </tr>
-        );
-    };
+const CourseListRow = ({ isHeader = false, textFirstCell, textSecondCell = null }) => {
+  if (isHeader) {
+    return (
+      <tr>
+        {textSecondCell === null ? (
+          <th colSpan="2"
+              style={{ backgroundColor: '#deb5b545'}}
+              className={css(styles.firstRow, styles.header, styles.headerBorder, styles.cellSpacing)}>
+                {textFirstCell}
+          </th>
+        ) : (
+          <>
+            <th style={{ backgroundColor: '#f5f5f5ab' }}
+              className={css(styles.header, styles.secondRow, styles.headerBorder, styles.cellSpacing)}>
+                {textFirstCell}
+            </th>
+            <th style={{ backgroundColor: '#f5f5f5ab' }}
+            className={css(styles.header, styles.secondRow, styles.headerBorder, styles.cellSpacing)}>
+              {textSecondCell}
+            </th>
+          </>
+        )}
+      </tr>
+    );
+  } else {
+    return (
+      <tr>
+        <td className={css(styles.tableBorder, styles.cellSpacing)}>{textFirstCell}</td>
+        <td className={css(styles.tableBorder, styles.cellSpacing)}>{textSecondCell}</td>
+      </tr>
+    )
+  }
 };
 
 CourseListRow.propTypes = {
-    isHeader: PropTypes.bool,
-    textFirstCell: PropTypes.string.isRequired,
-    textSecondCell: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number
-    ])
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
 };
+// APHRODITE STYLES:
 
-CourseListRow.defaultProps = {
-    isHeader: false,
-    textSecondCell: null
-};
+const styles = StyleSheet.create({
+
+  table: {
+    marginTop: '20px',
+    width: '100%',
+    marginBottom: '20px',
+    borderCollapse: 'collapse'
+  },
+
+  header: {
+    padding: '12px',
+    textAlign: 'left',
+  },
+
+  headerBorder: {
+    border: '2px solid #6A7AC0',
+  },
+
+  tableBorder: {
+    border: '2px solid #6A7AC0',
+  },
+
+  centerText: {
+    textAlign: 'center',
+  },
+
+  leftText: {
+    textAlign: 'left',
+  },
+
+  firstRow: {
+    ':first-child': {
+      textAlign: 'center',
+    },
+  },
+
+  secondRow: {
+    ':nth-child(2)': {
+      textAlign: 'left',
+    },
+  },
+
+  cellSpacing: {
+    padding: '15px',
+  },
+});
 
 export default CourseListRow;
