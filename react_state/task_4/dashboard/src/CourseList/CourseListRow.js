b763684131190ca4f4e42867b7d05badc755f8b4
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-01-31 20:38:43 -0600

    added react hooks

--- react_state/task_4/dashboard/src/CourseList/CourseListRow.js
+++ react_state/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,40 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
-const CourseListRow = ({ isHeader = false, textFirstCell, textSecondCell = null }) => {
-  if (isHeader) {
-    return (
-      <tr>
-        {textSecondCell === null ? (
-          <th colSpan="2"
-              style={{ backgroundColor: '#deb5b545'}}
-              className={css(styles.firstRow, styles.header, styles.headerBorder, styles.cellSpacing)}>
-                {textFirstCell}
-          </th>
-        ) : (
-          <>
-            <th style={{ backgroundColor: '#f5f5f5ab' }}
-              className={css(styles.header, styles.secondRow, styles.headerBorder, styles.cellSpacing)}>
-                {textFirstCell}
-            </th>
-            <th style={{ backgroundColor: '#f5f5f5ab' }}
-            className={css(styles.header, styles.secondRow, styles.headerBorder, styles.cellSpacing)}>
-              {textSecondCell}
-            </th>
-          </>
-        )}
-      </tr>
-    );
+const styles = StyleSheet.create({
+
+  headerRowSpan2: {
+    textAlign: 'center',
+  },
+
+  secondHeaderRow: {
+    borderTop: '2px solid #e0e0e0',
+    borderBottom: '2px solid #e0e0e0',
+  },
+
+  secondHeaderRowFirstCell: {
+    textAlign: 'left',
+    width: '70%',
+  },
+
+  secondHeaderRowSecondCell: {
+    textAlign: 'left',
+    width: '30%',
+  },
+
+  bodyRowCell: {
+    textAlign: 'left',
+  },
+
+  row: {
+    backgroundColor: '#f5f5f5ab',
+  },
+
+  header: {
+    backgroundColor: '#deb5b545',
+  },
+
+  rowChecked: {
+    backgroundColor: '#e6e4e4',
+  },
+});
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const rowStyle = css(
+    isHeader ? styles.header : styles.row,
+    !isHeader && isChecked && styles.rowChecked
+  );
+
+  if(isHeader) {
+    if(textSecondCell === null) {
+      return (
+        <tr className={rowStyle}>
+          <th colSpan="2">{textFirstCell}</th>
+        </tr>
+      );
+    } else {
+      return (
+        <tr className={rowStyle}>
+          <th className={css(styles.secondHeaderRowFirstCell)}>{textFirstCell}</th>
+          <th className={css(styles.secondHeaderRowSecondCell)}>{textSecondCell}</th>
+        </tr>
+      );
+    }
+
   } else {
     return (
-      <tr>
-        <td className={css(styles.tableBorder, styles.cellSpacing)}>{textFirstCell}</td>
-        <td className={css(styles.tableBorder, styles.cellSpacing)}>{textSecondCell}</td>
+      <tr className={rowStyle}>
+        <td className={css(styles.bodyRowCell)}>
+          <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} />
+          {textFirstCell}
+        </td>
+        <td className={css(styles.bodyRowCell)}>{textSecondCell}</td>
       </tr>
-    )
+    );
   }
-};
+}
 
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
@@ -44,53 +89,10 @@ CourseListRow.propTypes = {
     PropTypes.number,
   ]),
 };
-// APHRODITE STYLES:
 
-const styles = StyleSheet.create({
-
-  table: {
-    marginTop: '20px',
-    width: '100%',
-    marginBottom: '20px',
-    borderCollapse: 'collapse'
-  },
-
-  header: {
-    padding: '12px',
-    textAlign: 'left',
-  },
-
-  headerBorder: {
-    border: '2px solid #6A7AC0',
-  },
-
-  tableBorder: {
-    border: '2px solid #6A7AC0',
-  },
-
-  centerText: {
-    textAlign: 'center',
-  },
-
-  leftText: {
-    textAlign: 'left',
-  },
-
-  firstRow: {
-    ':first-child': {
-      textAlign: 'center',
-    },
-  },
-
-  secondRow: {
-    ':nth-child(2)': {
-      textAlign: 'left',
-    },
-  },
-
-  cellSpacing: {
-    padding: '15px',
-  },
-});
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+}
 
 export default CourseListRow;
