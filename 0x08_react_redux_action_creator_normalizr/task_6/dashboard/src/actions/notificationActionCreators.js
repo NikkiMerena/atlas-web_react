c1080647272d6817386fbe40fe91c39c0ab9e7b6
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-02-06 10:53:49 -0600

    task6 cont

--- 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/notificationActionCreators.js
@@ -1,4 +1,5 @@
 import { MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
+import { useDispatch } from 'react-redux';
 
 export const markAsRead = (index) => ({
   type: MARK_AS_READ,
@@ -9,3 +10,7 @@ export const setNotificationFilter = (filter) => ({
   type: SET_TYPE_FILTER,
   filter,
 });
+
+// Wrap the action creators with the dispatch function, to bound them together
+export const boundMarkAsRead = (index) => useDispatch(markAsRead(index));
+export const boundNotificationFilter = (filter) => useDispatch(setNotificationFilter(filter));
