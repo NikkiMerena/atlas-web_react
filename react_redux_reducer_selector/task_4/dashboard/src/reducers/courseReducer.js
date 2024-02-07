9620acfec841b3632516340bc0813f4b1b2b8ec6
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-02-07 13:26:16 -0600

    intergrated immutable.js

--- react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.js
+++ react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.js
@@ -1,23 +1,30 @@
-import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from "../actions/courseActionTypes";
+import { fromJS } from 'immutable';
+import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActionTypes';
+import { coursesNormalizer } from '../schema/courses';
 
-// Initial state
-const initialState = [];
+// Initial state using Immutable.js Map
+const initialState = fromJS({
+  entities: {
+    courses: {}
+  },
+  result: []
+});
 
-// Reducer function
 const courseReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_COURSE_SUCCESS:
-      return action.data.map(course => ({ ...course, isSelected: false }));
+      // Normalize the data and merge it with the state
+      // eslint-disable-next-line no-case-declarations
+      const normalizedData = coursesNormalizer(action.data);
+      return state.mergeDeep(normalizedData);
 
     case SELECT_COURSE:
-      return state.map(course =>
-        course.id === action.index ? { ...course, isSelected: true } : course
-        );
+      // Use the setIn function from Immutable to update the value of the item
+      return state.setIn(['entities', 'courses', action.index, 'isSelected'], true);
 
     case UNSELECT_COURSE:
-      return state.map(course =>
-        course.id === action.index ? { ...course, isSelected: false } : course
-        );
+      // Use the setIn function from Immutable to update the value of the item
+      return state.setIn(['entities', 'courses', action.index, 'isSelected'], false);
 
     default:
       return state;
