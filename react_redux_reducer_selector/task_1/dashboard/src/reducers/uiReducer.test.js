cecb109ac7f756076792f664fea9020e8009b652
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-02-06 20:30:16 -0600

    adding test

--- react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -1,31 +1,29 @@
 import uiReducer from './uiReducer';
 import { displayNotificationDrawer } from '../actions/uiActionCreators';
+import { Map } from 'immutable';
 
 describe('uiReducer', () => {
+  const initialState = Map({
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: Map({})
+  });
+
   it('should return the initial state when no action is passed', () => {
-    expect(uiReducer(undefined, {})).toEqual({
-      isNotificationDrawerVisible: false,
-      isUserLoggedIn: false,
-      user: {}
-    });
+    const state = uiReducer(undefined, {});
+    expect(state.equals(initialState)).toBe(true);
   });
 
   it('should return the initial state when an unrelated action is passed', () => {
-    expect(uiReducer(undefined, { type: 'SELECT_COURSE' })).toEqual({
-      isNotificationDrawerVisible: false,
-      isUserLoggedIn: false,
-      user: {}
-    });
+    const state = uiReducer(undefined, { type: 'SELECT_COURSE' });
+    expect(state.equals(initialState)).toBe(true);
   });
 
   it('should handle DISPLAY_NOTIFICATION_DRAWER', () => {
-    expect(uiReducer(undefined, displayNotificationDrawer())).toEqual({
-      isNotificationDrawerVisible: true,
-      isUserLoggedIn: false,
-      user: {}
-    });
+    const state = uiReducer(undefined, displayNotificationDrawer());
+    const expectedState = initialState.set('isNotificationDrawerVisible', true);
+    expect(state.equals(expectedState)).toBe(true);
   });
 
   // More tests for other actions...
 });
-
