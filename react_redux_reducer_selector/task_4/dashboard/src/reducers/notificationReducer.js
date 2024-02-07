ad0884e2e87c51dc7c405c44b0737f06d55dc7c7
Nikki <108352995+NikkiMerena@users.noreply.github.com>
2024-02-07 13:32:48 -0600

    integrated immmutable.js

--- react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,31 +1,31 @@
+import { fromJS } from 'immutable';
 import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from '../actions/notificationActionTypes';
+import { notificationsNormalizer } from '../schema/notifications';
 
-const initialState = {
-  notifications: [],
+// Initial state using Immutable.js Map
+const initialState = fromJS({
+  entities: {
+    notifications: {}
+  },
+  result: [],
   filter: "DEFAULT"
-};
+});
 
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_NOTIFICATIONS_SUCCESS:
-      return {
-        ...state,
-        notifications: action.data.map(notification => ({ ...notification, isRead: false }))
-      };
+      // Normalize the data and merge it with the state
+      // eslint-disable-next-line no-case-declarations
+      const normalizedData = notificationsNormalizer(action.data);
+      return state.mergeDeep(normalizedData);
 
     case MARK_AS_READ:
-      return {
-        ...state,
-        notifications: state.notifications.map(notification =>
-          notification.id === action.index ? { ...notification, isRead: true } : notification
-        )
-      };
+      // Use the setIn function from Immutable to update the value of the item
+      return state.setIn(['entities', 'notifications', action.index, 'isRead'], true);
 
     case SET_TYPE_FILTER:
-      return {
-        ...state,
-        filter: action.filter
-      };
+      // Use the set function from Immutable to update the value of the filter
+      return state.set('filter', action.filter);
 
     default:
       return state;
